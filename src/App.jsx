4bb9eadabba5065552170c776d8e7c0f11c43d7c
Deftly <deftkz@gmail.com>
2023-11-15 20:17:15 -0800

    reusable styled components

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -14,14 +14,30 @@ const App = () => {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Reporting Dashboard</Heading>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Check in")}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
 
-        <Heading as="h2">Buttons</Heading>
-        <Button onClick={() => alert("Upgraded")}>Upgrade</Button>
-        <Button onClick={() => alert("Build triggered")}>Build</Button>
-
-        <Heading as="h3">Forms</Heading>
-        <Input type="number" placeholder="Number of Upgrades" />
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of Guests" />
+              <Input type="number" placeholder="Number of Guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
