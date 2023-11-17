28b00f9e2ce1960228b6e9309bb4f5d19ee5e9c9
Deftly <deftkz@gmail.com>
2023-11-16 19:43:38 -0800

    sidebar and basic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,15 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
-            <Route index element={<Navigate to="dashboard" />} />
-            <Route path="account" element={<Account />} />
-            <Route path="bookings" element={<Bookings />} />
-            <Route path="cabins" element={<Cabins />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="users" element={<Users />} />
+            <Route index element={<Navigate to="/dashboard" />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/bookings" element={<Bookings />} />
+            <Route path="/cabins" element={<Cabins />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/users" element={<Users />} />
           </Route>
-          <Route path="login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
